1ed81bfc44ba1ebe3a81332d5c03f5a4def0a612
Aadil Raja <aadil2raja@gmail.com>
2024-08-15 19:40:03 +0500

    commiting

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import About from './components/About';
+import Portfolio  from './components/Portfolio';
 function App() {
   return (
    <div>
    <NavBar/>
    <Home/>
+   <About/>
+   <Portfolio/>
    </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div name="about" className="h-screen w-full text-white bg-gradient-to-b from-black to-gray-800">
+        <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            <div className="text-4xl font-bold inline">
+                <p>About</p>
+            </div>
+           <p className="text-xl mt-20">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit nostrum atque voluptates expedita placeat sed, dolore quisquam facere, impedit a, eaque ab molestias cumque amet? Libero modi ipsa magnam autem minus, aspernatur numquam harum veritatis nobis neque quaerat tempora sunt, commodi necessitatibus explicabo et reprehenderit consectetur corporis doloremque? Natus, libero.
+            </p>  
+        </div>
+        </div>
+  )
+}
+export default About;
